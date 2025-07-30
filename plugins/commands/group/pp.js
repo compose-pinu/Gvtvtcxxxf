7886d8909f20befd0533fb31b0756882b2bf2dc8
compose-pinu <ntkhanok@gmail.com>
2025-07-31 02:39:44 +0600

    Update pp.js

--- plugins/commands/group/pp.js
+++ plugins/commands/group/pp.js
@@ -5,24 +5,27 @@ const config = {
   usePrefix: false,
   usage: "[mention | reply | uid | facebook link]",
   cooldown: 0,
-  description: "profile picture of a user",
-  credits: "SK-SIDDIK-KHAN",
+  description: "Fetch profile picture of a user",
+  credits: "SK-SIDDIK-KHAN"
 };
 
-async function onCall({ message, args, event, usersData }) {
+async function onCall({ message, args, event = {}, usersData }) {
   const getAvatarUrl = async (uid) => await usersData.getAvatarUrl(uid);
-  let uid = event?.senderID;
+  let uid = event.senderID;
   let avt;
 
   try {
-    if (event.type === "message_reply") {
+    if (event.messageReply?.senderID) {
       uid = event.messageReply.senderID;
-    } else if (Object.keys(event.mentions || {}).length > 0) {
+
+    } else if (event.mentions && Object.keys(event.mentions).length > 0) {
       uid = Object.keys(event.mentions)[0];
+
     } else if (args.join(" ").includes("facebook.com")) {
-      const match = args.join(" ").match(/(\d+)/);
+      const match = args.join(" ").match(/(\d{5,})/);
       if (match) uid = match[0];
       else throw new Error("Invalid Facebook URL.");
+
     } else if (args[0]) {
       uid = args[0];
     }
@@ -30,7 +33,11 @@ async function onCall({ message, args, event, usersData }) {
     avt = await getAvatarUrl(uid);
     const stream = await global.utils.getStreamFromURL(avt);
 
-    return message.reply({ body: "", attachment: stream });
+    return message.reply({
+      body: ``,
+      attachment: stream
+    });
+
   } catch (error) {
     return message.reply(`⚠️ Error: ${error.message}`);
   }
