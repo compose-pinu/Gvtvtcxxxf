3981c2855675b76fe15107f7653ca1efbb3323be
compose-pinu <ntkhanok@gmail.com>
2025-07-31 02:53:45 +0600

    Update pp.js

--- plugins/commands/group/pp.js
+++ plugins/commands/group/pp.js
@@ -2,39 +2,42 @@ const config = {
   name: "pp",
   aliases: ["profile", "p"],
   permissions: [0],
-  usePrefix: false,
   usage: "[mention | reply | uid | facebook link]",
   cooldown: 0,
   description: "Fetch profile picture of a user",
-  credits: "SK-SIDDIK-KHAN"
+  credits: "SK-SIDDIK-KHAN",
+  usePrefix: false  // true করলে prefix লাগবে, false করলে natural message detect হবে
 };
 
-async function onCall({ message, args, event = {}, usersData }) {
-  const getAvatarUrl = async (uid) => await usersData.getAvatarUrl(uid);
+async function onCall({ message, args, event = {} }) {
+  // Access usersData controller
+  const usersData = global.controllers?.usersData?.();
+  if (!usersData || typeof usersData.getAvatarUrl !== "function") {
+    return message.reply("❌ Error: usersData system not found.");
+  }
+
   let uid = event.senderID;
-  let avt;
 
   try {
     if (event.messageReply?.senderID) {
       uid = event.messageReply.senderID;
-
     } else if (event.mentions && Object.keys(event.mentions).length > 0) {
       uid = Object.keys(event.mentions)[0];
-
     } else if (args.join(" ").includes("facebook.com")) {
       const match = args.join(" ").match(/(\d{5,})/);
       if (match) uid = match[0];
       else throw new Error("Invalid Facebook URL.");
-
     } else if (args[0]) {
       uid = args[0];
     }
 
-    avt = await getAvatarUrl(uid);
+    const avt = usersData.getAvatarUrl(uid);
+    if (!avt) throw new Error("Could not get avatar URL.");
+
     const stream = await global.utils.getStreamFromURL(avt);
 
     return message.reply({
-      body: ``,
+      body: `🖼️ Profile picture of UID: ${uid}`,
       attachment: stream
     });
 
